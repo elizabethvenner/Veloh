dc49b83d355ff3d57bf276b29a72ac46a2d57744
Elizabeth Venner <elizabeth.venner@kurtgeiger.com>
2017-06-10 22:11:34 +0100

    docking station docks instances of bikes and saves them as an attribute reader

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,7 +11,22 @@ describe DockingStation do
     it 'releases working bikes' do
       bike = subject.release_bike
       expect(bike).to be_working
-      end
+    end
+
+    it 'dock something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+    end
+
+    it { is_expected.to respond_to(:bike) }
+
+
   end
 
 end
