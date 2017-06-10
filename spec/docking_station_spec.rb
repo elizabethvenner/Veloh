d94cd1374bffd39d89d8b6b4d7f4e6f2bac08822
Elizabeth Venner <elizabeth.venner@kurtgeiger.com>
2017-06-10 21:49:22 +0100

    get docking station to returna  working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,17 @@
 require 'docking_station'
 
 describe DockingStation do
-  # station = DockingStation.new
+  station = DockingStation.new
 
   describe '#release_bike' do
     it 'releases a bike' do
       expect(subject).to respond_to(:release_bike)
     end
+
+    it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+      end
   end
 
 end
